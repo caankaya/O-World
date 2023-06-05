9b8a3c68cb21ed3b452827d1369650ae3772bc5a
Canoral <kaya68@outlook.fr>
2023-06-05 13:33:37 +0200

    build : :construction: Set up sidebar structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import SideBar from '@/components/SideBar';
 import WorldMap from '@/components/WorldMap';
 
 export default function Home() {
   return (
-    <main className="min-h-screen">
+    <main className="min-h-screen m-auto">
+      <SideBar />
       <WorldMap />
     </main>
   );
