d740816d718c06a09e9d06c3126e51689eeeb89b
leo-turco <leo.turco.83@gmail.com>
2023-06-06 19:43:54 +0200

    feat: :lipstick: Login and register modal styles

--- src/components/LoginModal.tsx
+++ src/components/LoginModal.tsx
@@ -19,19 +19,19 @@ function LoginModal() {
   }
 
   return (
-    <dialog open={isLoginModalOpen} className="modal">
+    <dialog open={isLoginModalOpen} className="modal z-[1]">
       <form
         method="dialog"
-        className="modal-box space-y-4 md:space-y-6"
+        className="modal-box space-y-4 md:space-y-6 bg-primary-content/75"
         action="#"
       >
-        <h1 className="text-xl font-bold leading-tight tracking-tight primary md:text-2xl dark:text-white">
+        <h1 className="text-xl font-bold leading-tight tracking-tight  md:text-2xl text-primary">
           Sign in to your account
         </h1>
         <div>
           <label
             htmlFor="email"
-            className="block mb-2 text-sm font-medium primary dark:text-white"
+            className="block mb-2 text-sm font-medium text-white"
           >
             Email
           </label>
@@ -39,7 +39,7 @@ function LoginModal() {
             type="email"
             name="email"
             id="email"
-            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="shadow-sm bg-white border border-white text-neutral sm:text-sm rounded-lg focus:outline-none focus:ring-2 focus:ring-primary-focus block w-full p-2.5"
             placeholder="name@company.com"
             required
           />
@@ -47,7 +47,7 @@ function LoginModal() {
         <div>
           <label
             htmlFor="password"
-            className="block mb-2 text-sm font-medium primary  dark:text-white"
+            className="block mb-2 text-sm font-medium text-white"
           >
             Password
           </label>
@@ -56,7 +56,7 @@ function LoginModal() {
             name="password"
             id="password"
             placeholder="••••••••"
-            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="shadow-sm bg-white border border-white text-neutral sm:text-sm rounded-lg focus:outline-none focus:ring-2 focus:ring-primary-focus block w-full p-2.5"
             required
           />
         </div>
@@ -67,37 +67,34 @@ function LoginModal() {
                 id="remember"
                 aria-describedby="remember"
                 type="checkbox"
-                className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                className="w-4 h-4 border border-white rounded bg-white"
                 required
               />
             </div>
             <div className="ml-3 text-sm">
-              <label
-                htmlFor="remember"
-                className="text-gray-500 dark:text-gray-300"
-              >
+              <label htmlFor="remember" className="text-white">
                 Remember me
               </label>
             </div>
           </div>
           <a
             href="#"
-            className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
+            className="text-sm font-medium text-white hover:underline"
           >
             Forgot password?
           </a>
         </div>
         <button
           type="submit"
-          className="w-full text-white bg-blue-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-primary-800"
+          className="w-full text-white bg-primary hover:bg-primary-focus focus:ring-4 focus:outline-none focus:ring-primary-focus font-medium rounded-lg text-sm px-5 py-2.5 text-center"
         >
           Sign in
         </button>
-        <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+        <p className="text-sm font-light text-white">
           Don’t have an account yet?{' '}
           <a
             href="#"
-            className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+            className="font-medium text-white hover:underline"
             onClick={changeAuthModalsinForm}
           >
             Sign up
--- tailwind.config.js
+++ tailwind.config.js
@@ -18,8 +18,7 @@ module.exports = {
 
   // daisyUI config (optional - here are the default values)
   daisyui: {
-    themes: ["light", "night"], // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
-    darkTheme: 'night', // name of one of the included themes for dark mode
+    themes: ['night'], // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
     base: false, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
     utils: true, // adds responsive and modifier utility classes
@@ -54,4 +53,3 @@ warning : #f4c152
 warning-content : #312817
 error : #fb6f84
 error-content : #311c1e */
-
