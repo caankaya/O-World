2dbf2543a5e9fc7e9f5ec8005341928dfc6b6e38
BENJAMIN GUIGANTON <benjamin.guiganton@gmail.com>
2023-06-06 15:26:37 +0900

    docs: :memo: Comments on the complex features like animated Stars and Animated text

--- src/components/Stars.jsx
+++ src/components/Stars.jsx
@@ -1,10 +1,10 @@
 'use client';
 
-import { useState, useRef, Suspense } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
-import { Points, PointMaterial, Preload } from "@react-three/drei";
+import { useState, useRef, Suspense } from "react"; //  Suspense est un composant qui vous permet de "suspendre" le rendu de certaines parties du composant jusqu'à ce qu'une condition soit remplie 
+import { Canvas, useFrame } from "@react-three/fiber"; // On importe ici les composants de la bibliothèque React pour travailler avec Three.js, Canvas est le composant principal qui crée une scène 3D, et useFrame est un Hook qui permet d'exécuter du code à chaque image de l'animation de la scène 3D
+import { Points, PointMaterial, Preload } from "@react-three/drei"; // Points est un composant qui représente un nuage de points dans la scène 3D, PointMaterial est un matériau que on utilisze pour rendre ces points, et Preload est un utilitaire qui précharge toutes les ressources 3D.
 
-function randomInSphere(count, radius) {
+function randomInSphere(count, radius) { // Cette fonction génère un certain nombre (count) de points aléatoires à l'intérieur d'une sphère de rayon donné (radius). Les points sont uniformément distribués dans la sphère. Les coordonnées de chaque point sont stockées dans un Float32Array
   let positions = new Float32Array(count * 3);
   for (let i = 0; i < count; i++) {
     let r = radius * Math.cbrt(Math.random());  // Cube root to get uniform distribution
@@ -17,7 +17,7 @@ function randomInSphere(count, radius) {
   return positions;
 }
 
-const Stars = (props) => {
+const Stars = (props) => { // Ceci est le composant principal qui génère les étoiles. Il utilise useRef pour conserver une référence à l'objet Points, useState pour stocker les positions initiales des étoiles, et useFrame pour faire tourner les étoiles à chaque image.
   const ref = useRef();
   const [sphere] = useState(() => randomInSphere(5000, 1.2));
 
@@ -27,21 +27,21 @@ const Stars = (props) => {
   });
 
   return (
-    <group rotation={[0, 0, Math.PI / 4]}>
-      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
+    <group rotation={[0, 0, Math.PI / 4]}> 
+      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}> {/* composant Points qui crée le nuage de points (étoiles). Il utilise les positions générées par la fonction randomInSphere et passe d'autres props à travers. */}
         <PointMaterial
           transparent
           color='#f272c8'
           size={0.002}
           sizeAttenuation={true}
           depthWrite={false}
-        />
+        /> {/* composant PointMaterial qui définit le matériau des points. Il est transparent, de couleur rose, et a une taille de 0,002. */}
       </Points>
     </group>
   );
 };
 
-const StarsCanvas = () => {
+const StarsCanvas = () => { // Ceci est le composant principal qui crée la scène 3D. Il utilise le composant Canvas de @react-three/fiber pour créer la scène, et le composant Stars pour créer le nuage de points.
   return (
     <div className="w-full h-auto absolute inset-0 z-[-1]">
       <Canvas camera={{ position: [0, 0, 1] }}>
--- src/utils/motion.js
+++ src/utils/motion.js
@@ -1,14 +1,14 @@
-import { motion, useAnimation } from "framer-motion";
+import { motion, useAnimation } from "framer-motion"; // npm i framer-motion
 import { useEffect, useState } from "react";
 
-const text = "dolor sit amet consectetur adipisicing elit";
-const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+const text = "dolor sit amet consectetur adipisicing elit"; 
+const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'; // Nous créons une chaîne de caractères qui contient tous les caractères que nous voulons utiliser pour animer le texte.
 
 const TextLetter = ({ children }) => {
   const controls = useAnimation();
   const [displayLetter, setDisplayLetter] = useState(children);
 
-  useEffect(() => {
+  useEffect(() => {// useEffect est un Hook qui permet d'exécuter du code à chaque fois que le composant est rendu. Ici, nous utilisons useEffect pour animer chaque lettre du texte.
     const animate = async () => {
       let counter = 0;
       while (counter < 10) {
@@ -16,7 +16,7 @@ const TextLetter = ({ children }) => {
           opacity: 1,
           transition: { duration: Math.random() + 0.5}
         });
-        setDisplayLetter(characters[Math.floor(Math.random() * characters.length)]);
+        setDisplayLetter(characters[Math.floor(Math.random() * characters.length)]); // Nous utilisons ce nombre pour sélectionner un caractère aléatoire de la chaîne characters.
         await controls.start({
           transition: { duration: Math.random() + 0.5 }
         });
@@ -25,28 +25,28 @@ const TextLetter = ({ children }) => {
     };
 
     animate();
-  }, [controls]);
+  }, [controls]); // useEffect est exécuté à chaque fois que controls change.
 
   return (
     <motion.span 
       initial={{ opacity: 0 }}
       animate={controls}
       className="alien-font text-center tracking-normal shadow-neon mx-2"
-      style={{ letterSpacing: "0.5em" }} 
-    >
+      style={{ letterSpacing: "0.2em" }} 
+    > {/* Nous utilisons le composant motion.span pour envelopper chaque lettre. */}
       {displayLetter === ' ' ? '\u00A0' : displayLetter}
     </motion.span>
   );
 };
 
-const AnimatedText = () => {
+const AnimatedText = () => { 
   return (
     <div className="">
       {Array.from(text).map((letter, i) => (
         <TextLetter key={i}>
           {letter === ' ' ? '\u00A0' : letter}
         </TextLetter>
-      ))}
+      ))} {/* Nous utilisons la méthode map pour créer un tableau de composants TextLetter. */}
     </div>
   );
 };
