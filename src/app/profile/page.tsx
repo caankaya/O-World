9e4d500390d1c0063701835f3666c66cfad4b5d6
leo-turco <leo.turco.83@gmail.com>
2023-06-07 23:29:30 +0200

    refactor: :recycle: Refactoring of the code with the creation of a Profile component including UpdateProfile and UserFavorites components (renaming of ProfilCard component to UpdateProfile)

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,10 +1,9 @@
 import Footer from '@/components/Footer';
 import LoginModal from '@/components/LoginModal';
 import NavBar from '@/components/NavBar';
-import ProfilCard from '@/components/ProfilCard';
+import Profile from '@/components/Profile';
 import SideBar from '@/components/SideBar';
 import StarsCanvas from '@/components/Stars';
-import UserFavorites from '@/components/UserFavorites';
 import { Stars } from '@react-three/drei';
 
 export default function Page() {
@@ -13,10 +12,7 @@ export default function Page() {
       <NavBar />
       <SideBar />
       <LoginModal />
-      <div className="flex items-center justify-center">
-        <ProfilCard />
-        <UserFavorites />
-      </div>
+      <Profile />
       {/* <StarsCanvas /> */}
       <Footer />
     </>
