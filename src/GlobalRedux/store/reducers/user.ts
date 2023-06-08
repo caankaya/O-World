d52cfcfd5be4799777491bbd7cd8759311231d44
Canoral <kaya68@outlook.fr>
2023-06-09 00:56:16 +0200

    feat: :sparkles: Change the status color when a user is  connected

--- src/GlobalRedux/store/reducers/user.ts
+++ src/GlobalRedux/store/reducers/user.ts
@@ -21,9 +21,9 @@ export const login = createAsyncThunk(
   'user/login',
   async (formInput: FormData) => {
     const obj = Object.fromEntries(formInput);
-    const response = await axiosInstance.post('/api/log/in', obj);
-    console.log('data :', response);
-    return response;
+    const { data } = await axiosInstance.post('/api/log/in', obj);
+    console.log('data :', data);
+    return data;
   }
 );
 
@@ -32,7 +32,7 @@ export const logout = createAction('user/logout');
 const userReducer = createReducer(initialState, (builder) => {
   builder
     .addCase(login.fulfilled, (state, action) => {
-      // state.pseudo = action.payload.user.username;
+      state.pseudo = action.payload.user.username;
       state.isLogged = true;
     })
     .addCase(logout, (state) => {
