4dac3d686819cca733055e538c5deb089512f6a9
BENJAMIN GUIGANTON <benjamin.guiganton@gmail.com>
2023-06-08 10:53:27 +0900

    build: :construction: AdminPage: Work in progress. Creation of the table with statics datas

--- src/components/Admin.tsx
+++ src/components/Admin.tsx
@@ -2,9 +2,10 @@
 
 import { useAppSelector } from '@/GlobalRedux/hooks';
 
-function Profile() {
+function Admin() {
   const AdminWidth = useAppSelector((state) => state.home.currentWidth);
   const isSideBarOpen = useAppSelector((state) => state.home.sideBar);
+
   return (
     <div
       className={`p-4 flex ${isSideBarOpen ? 'float-right' : ''}`}
@@ -13,73 +14,55 @@ function Profile() {
     <div className="container px-4 mx-auto w-full">
         <div className="flex flex-wrap -mx-4">
         <div className="w-full px-4">
-            <div className="pt-4 bg-white shadow rounded">
-            <div className="px-6 border-b border-blue-50">
+            <div className="pt-4 bg-neutral-content shadow rounded">
+            <div className="px-6 border-b border-neutral">
                 <div className="flex flex-wrap items-center mb-4">
                 <div>
-                    <h3 className="text-xl font-bold">Project Progress Data</h3>
-                    <p className="text-sm text-gray-500 font-medium">List of recent contracts and freelancers</p>
+                    <h3 className="text-xl text-base-100 font-bold">Statistics Admin Users</h3>
+                    <p className="text-sm text-base-100 font-medium">Users per country</p>
                 </div>
                 </div>
             </div>
             <div className="overflow-x-auto">
                 <table className="table-auto w-full">
-                <thead className="bg-gray-50">
-                    <tr className="text-xs text-gray-500 text-left">
-                    <th className="flex items-center pl-6 py-4 font-medium">Country</th>
-                    <th className="py-4 font-medium">Nombre utilisateur</th>
-                    <th className="py-4 font-medium">Progress</th>
+                <thead className="bg-neutral-focus">
+                    <tr className="text-md text-info-content text-left">
+                    <th className="flex items-center pl-6 py-4 font-bold text-info-content">Country</th>
+                    <th className="py-4 font-bold text-info-content">User total</th>
+                    <th className="py-4 font-bold text-info-content">percentage of total</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr className="border-b border-blue-50">
+                    <tr className="border-b border-neutral">
                     <td className="flex items-center py-4 px-6 font-medium">
-                        <input className="mr-3" type="checkbox" name="" id="" />
-                        <div className="flex px-4 py-3">
-                        <img className="w-8 h-8 mr-4 object-cover rounded-md" src="https://images.unsplash.com/photo-1559893088-c0787ebfc084?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1050&amp;q=80" alt="" />
-                        <div>
-                            <p className="text-sm font-medium">John Smith</p>
-                            <p className="text-xs text-gray-500">Freelancer</p>
-                        </div>
+                        <div className="flex px-4 py-3 items-center">
+                          <img className="w-8 h-8 mr-4 object-cover rounded-md" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Flag_of_France_%281794%E2%80%931815%2C_1830%E2%80%931974%2C_2020%E2%80%93present%29.svg/langfr-225px-Flag_of_France_%281794%E2%80%931815%2C_1830%E2%80%931974%2C_2020%E2%80%93present%29.svg.png" alt="" />
+                          <p className="text-md font-bold text-base-200">France</p>
                         </div>
                     </td>
-                    <td className="font-medium">
-                        <p className="text-sm">Example of Project</p>
-                        <p className="text-xs text-gray-500">New development</p>
+                    <td className="font-bold items-center">
+                        <p className="text-sm text-base-200">32000</p>
                     </td>
-                    <td className="pr-6">
-                        <p className="mb-1 text-xs text-indigo-500 font-medium">65%</p>
-                        <div className="flex">
-                        <div className="relative h-1 w-48 bg-indigo-50 rounded-full">
-                            <div className="absolute top-0 left-0 h-full w-2/6 bg-indigo-500 rounded-full"></div>
-                        </div>
-                        </div>
+                    <td className="font-bold items-center">
+                        <p className="text-sm text-base-200">65%</p>
                     </td>
                     </tr>
-                    <tr className="border-b border-blue-50">
+
+                    <tr className="border-b border-neutral">
                     <td className="flex items-center py-4 px-6 font-medium">
-                        <input className="mr-3" type="checkbox" name="" id="" />
-                        <div className="flex px-4 py-3">
-                        <img className="w-8 h-8 mr-4 object-cover rounded-md" src="https://images.unsplash.com/photo-1559893088-c0787ebfc084?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1050&amp;q=80" alt="" />
-                        <div>
-                            <p className="text-sm font-medium">John Smith</p>
-                            <p className="text-xs text-gray-500">Freelancer</p>
-                        </div>
+                        <div className="flex px-4 py-3 items-center">
+                          <img className="w-8 h-8 mr-4 object-cover rounded-md" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Flag_of_Japan.svg/langfr-225px-Flag_of_Japan.svg.png" alt="" />
+                          <p className="text-md font-bold text-base-200">Japan</p>
                         </div>
                     </td>
-                    <td className="font-medium">
-                        <p className="text-sm">Example of Project</p>
-                        <p className="text-xs text-gray-500">New development</p>
+                    <td className="font-bold items-center">
+                        <p className="text-sm text-base-200">32000</p>
                     </td>
-                    <td className="pr-6">
-                        <p className="mb-1 text-xs text-indigo-500 font-medium">65%</p>
-                        <div className="flex">
-                        <div className="relative h-1 w-48 bg-indigo-50 rounded-full">
-                            <div className="absolute top-0 left-0 h-full w-2/6 bg-indigo-500 rounded-full"></div>
-                        </div>
-                        </div>
+                    <td className="font-bold items-center">
+                        <p className="text-sm text-base-200">65%</p>
                     </td>
                     </tr>
+
                 </tbody>
                 </table>
                 <div className="py-4 text-center">
@@ -94,4 +77,4 @@ function Profile() {
   );
 }
 
-export default Profile;
+export default Admin;
