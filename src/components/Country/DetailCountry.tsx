28864f9de82bed19d1950c2b73d9f414935c0ec6
Canoral <kaya68@outlook.fr>
2023-06-09 21:50:33 +0200

    fix: :hammer: Configuring the width when the sidebar is open

--- src/components/Country/DetailCountry.tsx
+++ src/components/Country/DetailCountry.tsx
@@ -5,8 +5,13 @@ import Circulary from '@/components/Country/Circulary';
 import DashboardStats from '@/components/Country/DashboardStats';
 
 function Country() {
+  const isSideBarOpen = useAppSelector((state) => state.home.sideBar);
+  const width = useAppSelector((state) => state.home.currentWidth);
   return (
-    <div className={`page`}>
+    <div
+      className={`page`}
+      style={isSideBarOpen ? { width: width, float: 'right' } : {}}
+    >
       <DashboardStats />
       <BarChart />
       <Bilboards />
