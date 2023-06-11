485165d619a41b004aa0ae5025fe18cca73761d8
Canoral <kaya68@outlook.fr>
2023-06-11 02:19:30 +0200

    refactor: :reycle: DetailCountry component

--- src/components/Country/Bilboards.tsx
+++ src/components/Country/Bilboards.tsx
@@ -14,20 +14,20 @@ function Bilboards({ category, data }: BilboardsProps) {
         <div className="grid lg:grid-cols-2 mt-10 grid-cols-1 gap-6">
           <div className="stats bg-base-100 shadow">
             <div className="stat">
-              <div className="stat-title">Flag</div>
+              <div className="stat-title language mb-1">
+                What are they talking about ?
+              </div>
               <div className="stat-value">
-                <img
-                  src={data.flags.png}
-                  className="h-6 mr-3 sm:h-7"
-                  alt={data.flags.alt}
-                />
+                Language is {Object.values(data.languages)[0]}{' '}
               </div>
               <div className="stat-actions">
-                Language is {Object.values(data.languages)[0]}{' '}
+                They don't understand the extraterrestrial language
               </div>
             </div>
             <div className="stat">
-              <div className="stat-title">Currency</div>
+              <div className="stat-title currency mb-2">
+                What is their currency symbol ?
+              </div>
               <div className="stat-value">
                 {Object.values(data.currencies)[0]?.symbol}
               </div>
@@ -40,58 +40,23 @@ function Bilboards({ category, data }: BilboardsProps) {
           </div>
           <div className="stats bg-base-100 shadow">
             <div className="stat">
-              <div className="stat-figure invisible md:visible">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                  className="w-8 h-8"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-                  ></path>
-                </svg>
-              </div>
-              <div className="stat-title">Region</div>
+              <div className="stat-figure invisible md:visible"></div>
+              <div className="stat-title">What continent are they in ?</div>
               <div className="stat-value">{data.region}</div>
-              <div className="stat-desc">{data.subregion}</div>
+              <div className="stat-desc subregion mt-1">{data.subregion}</div>
             </div>
             <div className="stat">
-              <div className="stat-figure invisible md:visible">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                  className="w-8 h-8"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z"
-                  ></path>
-                </svg>
-              </div>
-              <div className="stat-title">Army</div>
+              <div className="stat-figure invisible md:visible"></div>
+              <div className="stat-title">The army flag</div>
               <div className="stat-value">
                 <img
                   src={data.coatOfArms.png}
                   className="h-6 mr-3 sm:h-7 block"
                   alt={data.flags.alt}
                   width={50}
-                  height={50}
                 />
               </div>
-              <div className="stat-desc">
-                {Object.values(data.name.nativeName)[0]?.official}
-              </div>
+              <div className="stat-desc">Make love ❤ No War &#128293;</div>
             </div>
           </div>
         </div>
--- src/components/Country/DashboardStats.tsx
+++ src/components/Country/DashboardStats.tsx
@@ -19,10 +19,10 @@ function DashboardStats({ category, data }: DashboardStatsProps) {
             <div className="stat-title dark:text-slate-300">Name</div>
             <div className="stat-value dark:text-slate-300 text-primary">
               <img
-                src={data.flags.svg}
+                src={data.flags.png}
                 alt={data.flags.alt}
                 width={50}
-                className="float-right"
+                className="float-right mt-2"
               />
               {data.name.common}
             </div>
@@ -46,23 +46,7 @@ function DashboardStats({ category, data }: DashboardStatsProps) {
         </div>
         <div className="stats shadow">
           <div className="stat">
-            <div className="stat-figure dark:text-slate-300 text-primary">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                aria-hidden="true"
-                className="w-8 h-8"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M20.25 6.375c0 2.278-3.694 4.125-8.25 4.125S3.75 8.653 3.75 6.375m16.5 0c0-2.278-3.694-4.125-8.25-4.125S3.75 4.097 3.75 6.375m16.5 0v11.25c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125V6.375m16.5 0v3.75m-16.5-3.75v3.75m16.5 0v3.75C20.25 16.153 16.556 18 12 18s-8.25-1.847-8.25-4.125v-3.75m16.5 0c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125"
-                ></path>
-              </svg>
-            </div>
+            <div className="stat-figure dark:text-slate-300 text-primary"></div>
             <div className="stat-title dark:text-slate-300">Capital</div>
             <div className="stat-value dark:text-slate-300 text-primary">
               {data.capital}
@@ -74,23 +58,7 @@ function DashboardStats({ category, data }: DashboardStatsProps) {
         </div>
         <div className="stats shadow">
           <div className="stat">
-            <div className="stat-figure dark:text-slate-300 text-primary">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                aria-hidden="true"
-                className="w-8 h-8"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z"
-                ></path>
-              </svg>
-            </div>
+            <div className="stat-figure dark:text-slate-300 text-primary"></div>
             <div className="stat-title dark:text-slate-300">Populations</div>
             <div className="stat-value dark:text-slate-300 text-primary">
               <CountUp
