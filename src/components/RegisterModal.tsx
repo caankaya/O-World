e6dabb3fbc3661c240ae2cf81b58ff226cdc989b
leo-turco <leo.turco.83@gmail.com>
2023-06-12 17:39:48 +0200

    refactor: :art: Clean code for the account creation form

--- src/components/RegisterModal.tsx
+++ src/components/RegisterModal.tsx
@@ -72,7 +72,7 @@ function RegisterModal() {
 
     // Check that the password entered by the user is secure
     const passwordRegex =
-      /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^\w\s]).{12,}$/;
+      /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^\w\s]).{8,}$/;
     if (!passwordRegex.test(password)) {
       //TODO Display a message on the front to warn the user
       console.log(
@@ -132,7 +132,7 @@ function RegisterModal() {
         </h1>
         <div>
           <label
-            htmlFor="text"
+            htmlFor="register-username"
             className="block mb-2 text-sm font-medium text-white"
           >
             Username
@@ -140,7 +140,7 @@ function RegisterModal() {
           <input
             type="text"
             name="username"
-            id="username"
+            id="register-username"
             className="shadow-sm bg-white border border-white text-neutral sm:text-sm rounded-lg focus:outline-none focus:ring-2 focus:ring-primary-focus block w-full p-2.5"
             placeholder="username"
             required
@@ -164,13 +164,13 @@ function RegisterModal() {
         </div>
         <div>
           <label
-            htmlFor="country_"
+            htmlFor="country-origin"
             className="block mb-2 text-sm font-medium text-white"
           >
             Country
           </label>
           <select
-            id="countries"
+            id="country-origin"
             name="country_origin"
             className="shadow-sm bg-white border border-white text-neutral sm:text-sm rounded-lg focus:outline-none focus:ring-2 focus:ring-primary-focus block w-full p-2.5"
             required
@@ -211,7 +211,7 @@ function RegisterModal() {
           >
             Password
             <span className="px-1 mb-2 text-xs text-white">
-              (+12 characters: upper case, number and special character)
+              (+8 characters: upper case, number and special character)
             </span>
           </label>
           <input
@@ -225,7 +225,7 @@ function RegisterModal() {
         </div>
         <div>
           <label
-            htmlFor="confirm-password"
+            htmlFor="confirm-register_password"
             className="block mb-2 text-sm font-medium text-white"
           >
             Confirm password
@@ -233,7 +233,7 @@ function RegisterModal() {
           <input
             type="password"
             name="confirm-password"
-            id="confirm-password"
+            id="confirm-register-password"
             placeholder="••••••••"
             className="shadow-sm bg-white border border-white text-neutral sm:text-sm rounded-lg focus:outline-none focus:ring-2 focus:ring-primary-focus block w-full p-2.5"
             required
