813b6da1d73de9e7cd7f418d79998079425b709e
leo-turco <leo.turco.83@gmail.com>
2023-06-12 15:03:17 +0200

    feat: :sparkles: User account modification functionality added to profile page

--- src/components/UpdateProfile.tsx
+++ src/components/UpdateProfile.tsx
@@ -1,11 +1,72 @@
 'use client';
 
+import { useAppSelector } from '@/GlobalRedux/hooks';
+import axios from '@/utils/axios';
+
+import { useRef } from 'react';
+
 function UpdateProfile() {
+  const userId = useAppSelector((state) => state.user.sessionId);
+
+  // Set up a useRef to target and reset the form
+  const newformRef = useRef<HTMLFormElement | null>(null);
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const newFormData = new FormData(event.currentTarget);
+
+    // Check that the password entered by the user and the password confirmation are identical
+    const newPassword = newFormData.get('password') as string;
+    const confirmNewPassword = newFormData.get('confirm-password') as string;
+    if (newPassword !== confirmNewPassword) {
+      //TODO Display a message on the front to warn the user
+      console.log(
+        'The password confirmation does not match the password entered.'
+      );
+      return;
+    }
+
+    // Check that the password entered by the user is secure
+    const passwordRegex =
+      /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^\w\s]).{8,}$/;
+    if (!passwordRegex.test(newPassword)) {
+      //TODO Display a message on the front to warn the user
+      console.log(
+        'The password does not meet the required criteria: at least 8 characters, one upper case, one lower case and one special character.'
+      );
+      return;
+    }
+
+    // Delete confirmpassword value before saving user info in DB
+    newFormData.delete('confirm-password');
+
+    try {
+      const response = await axios.put(
+        `http://localhost:3000/api/user/${userId}`,
+        Object.fromEntries(newFormData)
+      );
+      console.log(response.data);
+
+      // Reset form after submission
+      if (newformRef.current) {
+        newformRef.current.reset();
+      }
+    } catch (error) {
+      console.log(
+        'The information provided does not allow you to update your account',
+        error
+      );
+    }
+  };
+
   return (
     <form
+      ref={newformRef}
       method=""
       className="space-y-4 md:space-y-6 p-8 bg-primary-content/50 rounded-lg shadow flex flex-col gap-5 w-3/5"
       action="#"
+      onSubmit={handleSubmit}
     >
       <div className="flex flex-col items-center">
         <div className="avatar pb-5">
@@ -31,7 +92,7 @@ function UpdateProfile() {
             <input
               type="text"
               name="username"
-              id="username"
+              id="updated-username"
               className="shadow-sm bg-white border border-white text-neutral sm:text-sm rounded-lg focus:outline-none focus:ring-2 focus:ring-primary-focus block w-full p-2.5"
               placeholder="username"
               required
@@ -47,7 +108,7 @@ function UpdateProfile() {
             <input
               type="email"
               name="email"
-              id="email"
+              id="new-email"
               className="shadow-sm bg-white border border-white text-neutral sm:text-sm rounded-lg focus:outline-none focus:ring-2 focus:ring-primary-focus block w-full p-2.5"
               placeholder="name@company.com"
               required
@@ -65,7 +126,7 @@ function UpdateProfile() {
             <input
               type="password"
               name="password"
-              id="password"
+              id="new-password"
               placeholder="••••••••"
               className="shadow-sm bg-white border border-white text-neutral sm:text-sm rounded-lg focus:outline-none focus:ring-2 focus:ring-primary-focus block w-full p-2.5"
               required
@@ -73,15 +134,15 @@ function UpdateProfile() {
           </div>
           <div>
             <label
-              htmlFor="confirm-password"
+              htmlFor="password"
               className="block mb-2 text-sm font-medium text-white"
             >
               Confirm new password
             </label>
             <input
-              type="confirm-password"
+              type="password"
               name="confirm-password"
-              id="confirm-password"
+              id="confirm-new-password"
               placeholder="••••••••"
               className="shadow-sm bg-white border border-white text-neutral sm:text-sm rounded-lg focus:outline-none focus:ring-2 focus:ring-primary-focus block w-full p-2.5"
               required
