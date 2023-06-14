064b998f2ff65f20a4c8b7c8d7a6362e79d39170
BENJAMIN GUIGANTON <benjamin.guiganton@gmail.com>
2023-06-14 15:43:30 +0900

    Fix: setloading homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from '@/GlobalRedux/hooks';
 import Footer from '@/components/Footer';
 import NavBar from '@/components/NavBar';
@@ -10,19 +10,17 @@ import WorldMap from '@/components/WorldMap';
 import { setLoading } from '@/GlobalRedux/store/reducers/home';
 import StarsCanvas from '@/components/Stars';
 import HyperspaceEffect from '@/components/HyperspaceEffect';
-import { Loader } from '@react-three/drei';
 import Alert from '@/components/Alert';
 
 export default function Home() {
-  const [loading, setLoading] = useState(true);
   const dispatch = useAppDispatch();
-  const isSideBarOpen = useAppSelector((state) => state.home.sideBar);
+  const loading = useAppSelector((state) => state.home.spinner);
   const alert = useAppSelector((state) => state.user.alert);
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      setLoading(false);
-    }, 5000);
+      dispatch(setLoading(false));
+    }, 3000);
 
     return () => clearTimeout(timer); // nettoyage
   }, [dispatch]);
