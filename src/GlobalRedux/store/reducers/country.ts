36ba7365587287f8b1cd1b0f1769bc79158a69e7
BENJAMIN GUIGANTON <benjamin.guiganton@gmail.com>
2023-06-15 15:11:52 +0900

    refactor: :refactor: fetch API from page and pass datas to children by props for Admin and World page

--- src/GlobalRedux/store/reducers/country.ts
+++ src/GlobalRedux/store/reducers/country.ts
@@ -1,31 +1,60 @@
 import { CountryCategories } from '@/@types/countryCategories';
 import { CountriesDataProps } from '../../../@types/countryData';
-import { createReducer, createAction } from '@reduxjs/toolkit';
+import { AnyAction, ThunkDispatch, createAsyncThunk, createReducer } from '@reduxjs/toolkit';
+import { setError } from './error';
+import axiosInstance from '@/utils/axios';
 
 interface CountryState {
   category: CountryCategories[];
   data: CountriesDataProps | null;
+  loading: boolean;
 }
 
 const initialState: CountryState = {
   category: [],
   data: null,
+  loading: false,
 };
 
-export const setCountryCategory = createAction<CountryCategories[]>(
-  'country/categoryData'
-);
-export const setCountryData = createAction<CountriesDataProps>(
-  'country/countryData'
+export const fetchCountryData = createAsyncThunk<any, { id: string }, { dispatch: ThunkDispatch<{}, {}, AnyAction> }>(
+  'country/fetchCountryData',
+  async ({ id }, { dispatch }) => {
+    try {
+      const categoryUrl = `/oworld/${id}/category`;
+      const dataUrl = `/oworld/${id}`;
+
+      const categoryResponse = await axiosInstance.get(categoryUrl);
+      const dataResponse = await axiosInstance.get(dataUrl);
+
+      const categoryData = categoryResponse.data;
+      const countryData = dataResponse.data;
+
+      return { categoryData, countryData };
+
+    } catch (error: any) {
+      console.log('Error:', error);
+      dispatch(setError({ 
+        code: error.code, 
+        statusCode: error.response?.status || 500,
+        message: error.message || 'Une erreur inattendue est survenue.'
+      }));
+      throw error;
+    }
+  }
 );
 
 const countryReducer = createReducer(initialState, (builder) => {
   builder
-    .addCase(setCountryCategory, (state, action) => {
-      state.category = action.payload;
+    .addCase(fetchCountryData.pending, (state) => {
+      state.loading = true;
     })
-    .addCase(setCountryData, (state, action) => {
-      state.data = action.payload;
+    .addCase(fetchCountryData.fulfilled, (state, action) => {
+      state.loading = false;
+      state.category = action.payload.categoryData;
+      state.data = action.payload.countryData;
+    })
+    .addCase(fetchCountryData.rejected, (state) => {
+      state.loading = false;
     });
 });
 
