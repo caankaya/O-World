423572292d498ea48f36ba5889e4dfebde27b1b8
BENJAMIN GUIGANTON <benjamin.guiganton@gmail.com>
2023-06-17 13:40:47 +0900

    style: :lipstick: animation effect on navbar button to display the sidebar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -92,18 +92,24 @@ const NavBar = () => {
         <nav
           className={`navbar bg-base-100 z-[1] bg-transparent flex items-center justify-between w-full`}
         >
-          <div className="flex mx-4">
-            <button aria-controls="logo-sidebar" type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          <div className="relative inline-block text-center">
+          <button
+            type="button"
+            className="focus:outline-none"
             onClick={() => {
-              dispatch(togglerSideBar(isSideBarOpen));
+              dispatch(togglerSideBar(!isSideBarOpen));
             }}
-            >
-              <span className="sr-only">Open sidebar</span>
-              <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                <path clipRule="evenodd" fillRule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
-              </svg>
+          >
+            <span
+              className={`block h-1 w-6 bg-info-content rounded-full transition-all duration-1000 transform ${isSideBarOpen ? "rotate-45 translate-y-2" : ""}`}
+            />
+            <span
+              className={`block h-1 w-6 bg-info-content rounded-full mt-1 transition-all duration-1000 ${isSideBarOpen ? "opacity-0" : ""}`}
+            />
+            <span
+              className={`block h-1 w-6 bg-info-content rounded-full mt-1 transition-all duration-1000 transform ${isSideBarOpen ? "-rotate-45 -translate-y-2" : ""}`}
+            />
           </button>
-
           </div>
           <div className="hidden md:block w-full text-center">
             <AnimatedText text="Voici la planète terre, berceau de l'humanité" />
