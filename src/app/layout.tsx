40d7e34bb5fc28030fee0fd84f1a26c1f6eae678
BENJAMIN GUIGANTON <benjamin.guiganton@gmail.com>
2023-06-18 10:55:20 +0900

    style: :lipstick: sidebar upgrade

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/logo-oworld.png" />
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
       </Head>
