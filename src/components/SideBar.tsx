96419c5a8d1b7446ed0c77de2407f4da2da5d90d
BENJAMIN GUIGANTON <benjamin.guiganton@gmail.com>
2023-06-20 10:49:17 +0900

    fix: :bug: svg in sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -281,7 +281,7 @@ function SideBar() {
                   xmlns="http://www.w3.org/2000/svg" 
                   fill="none" 
                   viewBox="0 0 24 24" 
-                  stroke-width="1.5" 
+                  strokeWidth="1.5" 
                   stroke="currentColor" 
                   className="w-6 h-6 mr-5"
                 >
@@ -309,7 +309,7 @@ function SideBar() {
                   xmlns="http://www.w3.org/2000/svg" 
                   fill="none" 
                   viewBox="0 0 24 24" 
-                  stroke-width="1.5" 
+                  strokeWidth="1.5" 
                   stroke="currentColor" 
                   className="w-6 h-6 mr-5">
                   <path 
