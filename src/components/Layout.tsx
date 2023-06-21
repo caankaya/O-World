c032bc767c49845d3438cc2c3ba137d63e053ff3
BENJAMIN GUIGANTON <benjamin.guiganton@gmail.com>
2023-06-21 17:10:57 +0900

    build: :construction: creation des composants toast et alert et ajout du toast dans la sidebar

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,12 +3,14 @@ import Footer from './Footer';
 import NavBar from './NavBar';
 import SideBar from './SideBar';
 import StarsCanvas from './Stars';
+import Toast from './Toast';
 
 function Layout() {
   return (
     <div className="app">
       <NavBar />
       <SideBar />
+      <Toast />
 
       <Outlet />
 
