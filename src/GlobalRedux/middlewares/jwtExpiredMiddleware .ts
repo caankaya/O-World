a0d30eda4591b1817a1ad67faea433e193597955
leo-turco <leo.turco.83@gmail.com>
2023-06-26 19:49:18 +0200

    feat: :sparkles: Typing middleware jwtExpired

--- src/GlobalRedux/middlewares/jwtExpiredMiddleware .ts
+++ src/GlobalRedux/middlewares/jwtExpiredMiddleware .ts
@@ -1,8 +1,12 @@
+import { Middleware, Dispatch } from 'redux';
 import { refreshAccessToken } from '../store/reducers/user';
+import { ThunkAction } from 'redux-thunk';
 
-const jwtExpiredMiddleware = (store) => (next) => (action) => {
+const jwtExpiredMiddleware: Middleware = (store) => (next) => (action) => {
+  const dispatch: Dispatch<any> = store.dispatch;
+
   if (action.error && action.error.message === 'jwt expired') {
-    store.dispatch(refreshAccessToken());
+    dispatch(refreshAccessToken() as ThunkAction<void, any, any, any>);
   }
 
   return next(action);
